18044096ae70d3f55cf571c2f14b0e564ba32d64
thatchaseb <77036934+thatchaseb@users.noreply.github.com>
2024-01-29 14:14:41 -0500

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import Hero from "./components/Hero.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <Hero />
 </template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,32 @@
+<script setup>
+import DiscoverModal from "./DiscoverModal.vue";
+import SocialIcon from "./SocialIcon.vue";
+</script>
+
+<template>
+  <!--Main Grid-->
+  <div class="grid h-[40rem] grid-cols-12">
+    <!--Rose Background Grid-->
+    <div class="col-span-4 grid grid-cols-12 content-center bg-rose-950">
+      <!--Col Span Spacer-->
+      <div class="col-span-6"></div>
+
+      <!--Modal Container-->
+      <div class="col-span-6">
+        <DiscoverModal />
+      </div>
+    </div>
+
+    <!--Tree Background Grid-->
+    <div class="col-span-8 grid grid-cols-12 place-content-end bg-blue-400">
+      <!--Col Span Spacer-->
+      <div class="col-span-10"></div>
+
+      <!--Social Container-->
+      <div class="col-span-2">
+        <SocialIcon />
+        <SocialIcon />
+      </div>
+    </div>
+  </div>
+</template>
